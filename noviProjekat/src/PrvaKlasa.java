67dced29ea91ae431472522461cbb20658ab71fa
Ika2212 <ikamilenovic@gmail.com>
2024-03-23 17:39:49 +0100

    Novi prijekat prvi put

---
+++ noviProjekat/src/PrvaKlasa.java
@@ -0,0 +1,15 @@
+
+
+public class PrvaKlasa {
+        public static void main (String [] args) {
+                Kalkulator k1 = new Kalkulator(10.5D, 5.5D);
+
+                double rezultat = k1.saberi();
+
+                System.out.println(rezultat);
+
+        }
+}
+
+
+
---
+++ noviProjekat/src/PrvaKlasajava.java
@@ -0,0 +1,12 @@
+public class PrvaKlasajava {
+
+    public static void main (String [] args) {
+    Kalkulator k1 = new Kalkulator( 10.5D,  5.5D);
+
+    double rezultat = k1.oduzmi();
+
+        System.out.println(rezultat);
+
+    }
+}
+
