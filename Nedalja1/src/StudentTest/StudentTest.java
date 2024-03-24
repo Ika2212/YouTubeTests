1729b6bf6b5e2da1d62f02f32c7cf499dac384d2
Ika2212 <ikamilenovic@gmail.com>
2024-03-24 15:47:05 +0100

    Izmene

--- Nedalja1/src/StudentTest/StudentTest.java
+++ Nedalja1/src/StudentTest/StudentTest.java
@@ -1,4 +1,10 @@
 package StudentTest;
 
 public class StudentTest {
+
+    void  getIme() {
+        Student s = new Student(ime: "Ime studenta", prezime: "Prezime studenta",
+                brojindeksa"2014500100");
+
+        Assertions.assertEguals(expected: "Ime studenta", s.getIme());
 }
