9ec0446453cf8853a65699d830e190377dc88875
Ika2212 <ikamilenovic@gmail.com>
2024-03-24 13:24:44 +0100

    Izmene

---
+++ Nedalja1/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    void  getIme() {
+        Student s = new Student(ime: "Ime studenta", prezime: "Prezime studenta",
+                brojindeksa"2014500100");
+
+        Assertions.assertEguals(expected: "Ime studenta", s.getIme());
+
+
+    }
+
+}
