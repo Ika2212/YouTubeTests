1461dc8e1a6ba927991ee133df0b23e7762751d9
Ika2212 <ikamilenovic@gmail.com>
2024-03-25 20:12:18 +0100

    Izmene

--- Nedalja1/src/Student.java
+++ Nedalja1/src/Student.java
@@ -1,11 +1,46 @@
+import jdk.incubator.vector.VectorOperators;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 public class Student {
-    void  getIme() {
-        Student s = new Student(ime: "Ime studenta", prezime: "Prezime studenta",
-                brojindeksa"2014500100");
+    private final String Ime;
+    private final String Prezime;
+    private final String brojIndeksa;
+    private final List <VectorOperators.Test> radjeniTestovi
+    public Student(String Ime, String Prezime, String brojIndeksa ) {
+        this.Ime = Ime
 
-        Assertions.assertEguals(expected: "Ime studenta", s.getIme());
+        this.Prezime = Prezime
 
+        this.brojIndeksa = brojIndeksa
+
+        this.radjeniTestovi = new LinkedList<>()
+    }
+
+    public String getIme() {return Ime;}
+    public String getPrezime() {return Prezime;}
+    public String getBrojIndeksa() {return brojIndeksa;}
+
+    public void evidentirajRadjeniTest(VectorOperators.Test radjeni VectorOperators.Test)
+
+    {this.radjeniTestovi.add(radjeniTest);
+
+    public double getBrojPoenaZaPredmet(String predmet) {
+        final Optional<VectorOperators.Test> najnoviji =
+                this. radjeniTestovi
+                        .stream()
+                        .filter(test -> test.getImePredmeta().eguals(predmet))
+                        .sorted(r1, r2)-> r2 getDatumIso().compareTo(r1.getDatumIso()))
+                        .findFirst();
+if (!najnoviji.isPresent()) {
+return 0.;
+}
+return najnoviji.get().getBrojPoena(maksimum 30);
 
     }
 
 }
+
+
--- Nedalja1/src/StudentTest/StudentTest.java
+++ Nedalja1/src/StudentTest/StudentTest.java
@@ -1,10 +1,32 @@
 package StudentTest;
 
-public class StudentTest {
+import jdk.incubator.vector.VectorOperators;
 
+class StudentTest {
+    VectorOperators.Test
     void  getIme() {
-        Student s = new Student(ime: "Ime studenta", prezime: "Prezime studenta",
-                brojindeksa"2014500100");
+        Student s = new StudentTest( "Ime studenta", "Prezime studenta", "2014500100");
 
-        Assertions.assertEguals(expected: "Ime studenta", s.getIme());
-}
+        Assertions.assertEguals("Ime studenta", s.getIme());
+    }
+    VectorOperators.Test
+    void  getPrezime() {
+        Student s = new StudentTest( "Ime studenta",  "Prezime studenta", "2014500100");
+
+        Assertions.assertEguals("Prezime studenta", s.getPrezime());
+    }
+    VectorOperators.Test
+    void  getBrojIndeksa() {
+        Student s = new StudentTest( "Ime studenta",  "Prezime studenta", "2014500100");
+
+        Assertions.assertEguals("2014500100", s.getBrojIndeksa());
+
+    }
+    VectorOperators.Test
+    void  getBrojPoenaZaPredmet() {
+        Student s = new Student( "Ime studenta",  "Prezime studenta", "2014500100");
+
+        VectorOperators.Test
+
+
+
